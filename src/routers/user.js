b620a760d675bd68782068939e504f45901e3692
Vineet Kumar <vineet.kumar@vivaconnect.co>
2019-12-26 19:42:13 +0530

    changes in user

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,4 +15,15 @@ router.post('/createUser', async (req, res) => {
     }
 })
 
+
+router.get('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
 module.exports = router

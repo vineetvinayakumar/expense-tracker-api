d1f548d5cba360f7e5f848ce4dc5faa5e1b12427
Vineet Kumar <vineet.kumar@vivaconnect.co>
2019-12-27 18:41:19 +0530

    Read users api added

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const app = express()
 
+/* Create new User API Start */
 router.post('/createUser', async (req, res) => {
     const user = new User({
         ...req.body
     })
-
     try {
         await user.save()
         res.status(201).send(user)
@@ -14,16 +15,16 @@ router.post('/createUser', async (req, res) => {
         res.status(400).send(e)
     }
 })
+/* Create new User API End */
 
-
+/* Read all User API Start */
 router.get('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.status(201).send(user)
+    User.find({}).then((users) => {
+        res.send(users)
     }).catch((e) => {
-        res.status(400).send(e)
+        res.status(500).send()
     })
 })
+/* Read all User API End */
 
 module.exports = router
